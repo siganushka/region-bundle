9856e4fedd29f1ee2e9825f63ac6b60daf92257c
siganushka <siganushka@gmail.com>
2024-07-11 22:44:14 +0800

    refactor: improving event on Hotwire turbo render

--- src/Resources/public/main.js
+++ src/Resources/public/main.js
@@ -1,30 +1,35 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const change = async (event) => {
-    const { siganRegionTarget, siganRegionUrl } = event.target.dataset
-    const target = document.getElementById(siganRegionTarget)
-    if (target) {
-      const regions = []
+const change = async (event) => {
+  const { siganRegionTarget, siganRegionUrl } = event.target.dataset
+  const target = document.getElementById(siganRegionTarget)
+  if (target) {
+    const regions = []
 
-      const placeholder = target.querySelector('option[value=""]')
-      if (placeholder) {
-        regions.push({ code: '', name: placeholder.textContent })
-      }
-
-      if (event.target.value) {
-        const headers = { Accept: 'application/json' }
-        const response = await fetch(`${siganRegionUrl}?parent=${event.target.value}`, { headers })
-        regions.push(...await response.json())
-      }
-
-      const options = regions.map(el => `<option value="${el.code}">${el.name}</option>`)
-      target.innerHTML = options.join('')
-      target.dispatchEvent(new Event('change'))
+    const placeholder = target.querySelector('option[value=""]')
+    if (placeholder) {
+      regions.push({ code: '', name: placeholder.textContent })
     }
-  }
 
+    if (event.target.value) {
+      const headers = { Accept: 'application/json' }
+      const response = await fetch(`${siganRegionUrl}?parent=${event.target.value}`, { headers })
+      regions.push(...await response.json())
+    }
+
+    const options = regions.map(el => `<option value="${el.code}">${el.name}</option>`)
+    target.innerHTML = options.join('')
+    target.dispatchEvent(new Event('change'))
+  }
+}
+
+const connect = () => {
   const elements = document.querySelectorAll('[data-sigan-region-target]')
   elements.forEach(element => {
     element.addEventListener('change', change)
     // element.dispatchEvent(new Event('change'))
   })
-})
+}
+
+// Native event
+document.addEventListener('DOMContentLoaded', connect)
+// Hotwire turbo event
+document.addEventListener('turbo:render', connect)
