88916881d856018cb9b0853c36b73133380ec892
siganushka <siganushka@gmail.com>
2024-09-21 15:32:12 +0800

    tests: improving tests

--- tests/Routing/RoutesTest.php
+++ tests/Routing/RoutesTest.php
@@ -7,26 +7,33 @@ namespace Siganushka\RegionBundle\Tests\Routing;
 use PHPUnit\Framework\TestCase;
 use Symfony\Bundle\FrameworkBundle\Routing\AttributeRouteControllerLoader;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\Config\Loader\DelegatingLoader;
 use Symfony\Component\Config\Loader\LoaderResolver;
 use Symfony\Component\Routing\Loader\AttributeDirectoryLoader;
 use Symfony\Component\Routing\Loader\PhpFileLoader;
 
 class RoutesTest extends TestCase
 {
-    public function testRotues(): void
+    /**
+     * @dataProvider routesProvider
+     */
+    public function testRotues(string $routeName, string $path, array $methods): void
     {
         $locator = new FileLocator(__DIR__.'/../../config/');
 
-        $resolver = new LoaderResolver([
-            new PhpFileLoader($locator),
+        new LoaderResolver([
+            $loader = new PhpFileLoader($locator),
             new AttributeDirectoryLoader($locator, new AttributeRouteControllerLoader()),
         ]);
 
-        $loader = new DelegatingLoader($resolver);
         $routes = $loader->load('routes.php');
 
-        static::assertSame('/regions', $routes->get('siganushka_region_region_getcollection')->getPath());
-        static::assertSame('/regions/{code}', $routes->get('siganushka_region_region_getitem')->getPath());
+        static::assertSame($path, $routes->get($routeName)?->getPath());
+        static::assertSame($methods, $routes->get($routeName)?->getMethods());
+    }
+
+    public static function routesProvider(): iterable
+    {
+        yield ['siganushka_region_region_getcollection', '/regions', ['GET']];
+        yield ['siganushka_region_region_getitem', '/regions/{code}', ['GET']];
     }
 }
