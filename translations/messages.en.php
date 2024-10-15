c691cf9dde2a0c855da3843a46b0333d73541777
siganushka <siganushka@gmail.com>
2024-10-15 15:39:23 +0800

    refactor: improving trans message

---
+++ translations/messages.en.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'region.province' => 'Province',
+    'region.city' => 'City',
+    'region.district' => 'District',
+    'region.street' => 'Street',
+];
