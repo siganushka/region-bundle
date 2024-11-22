60ce6d28f188f5e182a72c2df9ce2792eecc618f
siganushka <siganushka@gmail.com>
2024-11-22 14:35:47 +0800

    refactor: improivng repository method return type

--- src/Repository/RegionRepository.php
+++ src/Repository/RegionRepository.php
@@ -9,12 +9,6 @@ use Siganushka\RegionBundle\Entity\Region;
 
 /**
  * @extends GenericEntityRepository<Region>
- *
- * @method Region      createNew(...$args)
- * @method Region|null find($id, $lockMode = null, $lockVersion = null)
- * @method Region|null findOneBy(array $criteria, array $orderBy = null)
- * @method Region[]    findAll()
- * @method Region[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class RegionRepository extends GenericEntityRepository
 {
