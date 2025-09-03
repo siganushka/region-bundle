cc8c198c4f1bc3bd8279e081f312b05fece3da56
siganushka <siganushka@gmail.com>
2025-09-03 17:26:55 +0800

    refactor: Using attribute route replace annotation route

--- src/Controller/RegionController.php
+++ src/Controller/RegionController.php
@@ -9,7 +9,7 @@ use Siganushka\RegionBundle\Repository\RegionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 class RegionController extends AbstractController
