6c9b5ed2530a2c95473a276e54dbfcf56e10f07b
siganushka <siganushka@gmail.com>
2026-04-07 17:41:12 +0800

    refactor: improve mapping override

--- src/DependencyInjection/SiganushkaRegionExtension.php
+++ src/DependencyInjection/SiganushkaRegionExtension.php
@@ -41,6 +41,10 @@ class SiganushkaRegionExtension extends Extension implements PrependExtensionInt
             }
         }
 
+        $container->prependExtensionConfig('doctrine', [
+            'orm' => ['resolve_target_entities' => $mappingOverride],
+        ]);
+
         $container->prependExtensionConfig('siganushka_generic', [
             'doctrine' => ['mapping_override' => $mappingOverride],
         ]);
