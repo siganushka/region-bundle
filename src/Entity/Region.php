6c45a0ea4cfb1ec5e2b2f05b0ca2bcceea70fb9b
siganushka <siganushka@gmail.com>
2026-05-27 16:29:37 +0800

    refactor: remove invalid attribute

--- src/Entity/Region.php
+++ src/Entity/Region.php
@@ -13,7 +13,6 @@ use Siganushka\RegionBundle\Repository\RegionRepository;
  * @extends Nestable<Region>
  */
 #[ORM\Entity(repositoryClass: RegionRepository::class, readOnly: true)]
-#[ORM\HasLifecycleCallbacks]
 #[ORM\Cache(usage: 'NONSTRICT_READ_WRITE')]
 class Region extends Nestable
 {
